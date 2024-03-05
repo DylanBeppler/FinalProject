d1a42e5d4e0b94130678eb1e73ff117937b9363c
Blake Vieyra <blake.vieyra@gmail.com>
2024-03-05 12:08:23 -0800

    pushing db change

--- JPAWoWContent/src/main/java/com/skilldistillery/wowcontent/entities/User.java
+++ JPAWoWContent/src/main/java/com/skilldistillery/wowcontent/entities/User.java
@@ -1,5 +1,126 @@
 package com.skilldistillery.wowcontent.entities;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+	public User() {
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String username;
+	private String email;
+	private String password;
+	private boolean enabled;
+	private String role;
+
+	@CreationTimestamp
+	@Column(name = "join_date")
+	private LocalDateTime joinDate;
+
+//	@JsonIgnore
+//	@OneToMany(mappedBy = "user")
+//	private List<Mount> todos;
+//	
+//	@JsonIgnore
+//	@OneToMany(mappedBy = "user")
+//	private List<Quest> quest;
+//	
+//	@JsonIgnore
+//	@OneToMany(mappedBy = "user")
+//	private List<Boss> boss;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	public LocalDateTime getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(LocalDateTime joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
+				+ ", enabled=" + enabled + ", role=" + role + ", joinDate=" + joinDate + "]";
+	}
+
 
 }
--- JPAWoWContent/src/test/java/com/skilldistillery/wowcontent/entities/UserTest.java
+++ JPAWoWContent/src/test/java/com/skilldistillery/wowcontent/entities/UserTest.java
@@ -8,27 +8,48 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class UserTest {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
+class UserTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
+
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAWoWContent");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_User_Has_Username() {
+		assertNotNull(user);
+		assertEquals("blake", user.getUsername());
+		
 	}
-
+	
+//	@Test
+//	void test_User_Has_Quests() {
+//		assertNotNull(user);
+//		assertTrue(user.getQuests().size() > 0);	
+//	}
 }
