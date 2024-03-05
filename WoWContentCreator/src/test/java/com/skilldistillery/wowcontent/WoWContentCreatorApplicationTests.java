fdbabdd57a03c54530eac26fb8d6d3ccaf62dfbe
DylanBeppler <dylanbeppler@gmail.com>
2024-03-05 11:42:56 -0600

    Create Spring Boot REST

---
+++ WoWContentCreator/src/test/java/com/skilldistillery/wowcontent/WoWContentCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.wowcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoWContentCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
