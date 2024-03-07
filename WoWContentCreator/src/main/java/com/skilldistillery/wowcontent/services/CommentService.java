73cac8277dc7decaa8d233ad7b0817bfdcff2ff9
Blake Vieyra <blake.vieyra@gmail.com>
2024-03-07 11:00:45 -0800

    comment backend stuff

--- WoWContentCreator/src/main/java/com/skilldistillery/wowcontent/services/CommentService.java
+++ WoWContentCreator/src/main/java/com/skilldistillery/wowcontent/services/CommentService.java
@@ -13,7 +13,7 @@ public interface CommentService {
 	public List <ContentComment> showCommentsByContentId(int contentId);
     public ContentComment create(String userName, ContentComment contentComment, int contentId);
     public ContentComment update(String userName, int contentId, int commentId, ContentComment contentComment);
-    public boolean destroy(String userName, int commentId);
+    public boolean destroy(String userName, int contentId, int commentId);
 	
 	
 }
