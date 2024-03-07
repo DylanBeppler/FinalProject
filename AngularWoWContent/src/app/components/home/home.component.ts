6907ccb778f3468bd49bbdc7e114311580f215b8
DylanBeppler <dylanbeppler@gmail.com>
2024-03-07 15:05:58 -0600

    dynamically added category divs to home page

--- AngularWoWContent/src/app/components/home/home.component.ts
+++ AngularWoWContent/src/app/components/home/home.component.ts
@@ -3,19 +3,42 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { ContentComponent } from '../content/content.component';
+import { ContentCategoryService } from '../../services/content-category.service';
+import { ContentCategory } from '../../models/content-category';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
   CommonModule,
-  FormsModule,
+  FormsModule
 
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  allContentCategories: ContentCategory[] = [];
+
+ ngOnInit(): void {
+  this.contentCategoryService.index().subscribe({
+    next: (contentCategories) => {
+      this.allContentCategories = contentCategories;
+    },
+    error: (problem) => {
+      console.error('ContentComponent.reload(): error loading all content categories: ');
+      console.error(problem);
+    },
+  });
+
+ }
+
+
+  constructor(
+  private contentCategoryService: ContentCategoryService
+) {}
+
 
 }
 
