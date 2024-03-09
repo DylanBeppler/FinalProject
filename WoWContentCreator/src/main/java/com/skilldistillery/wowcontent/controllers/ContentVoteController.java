badd0ed4132edd672a73d89584de1d93c59a60fb
DylanBeppler <dylanbeppler@gmail.com>
2024-03-09 13:59:12 -0600

    added vote controller index, create, update, and findbyid

--- WoWContentCreator/src/main/java/com/skilldistillery/wowcontent/controllers/ContentVoteController.java
+++ WoWContentCreator/src/main/java/com/skilldistillery/wowcontent/controllers/ContentVoteController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,33 +20,54 @@ import com.skilldistillery.wowcontent.entities.ContentVote;
 import com.skilldistillery.wowcontent.services.ContentService;
 import com.skilldistillery.wowcontent.services.ContentVoteService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
-@CrossOrigin({"*", "http://localhost/"})
+@CrossOrigin({ "*", "http://localhost/" })
 @RequestMapping("api")
 @RestController
 public class ContentVoteController {
-@Autowired 
-private ContentVoteService voteService;
-@Autowired
-private ContentService contentService;
+	@Autowired
+	private ContentVoteService voteService;
+	@Autowired
+	private ContentService contentService;
 
-	@GetMapping("content/{content_id}/votes")
-	public List <ContentVote> showVotesByContentId(@PathVariable("contentId") int contentId, HttpServletResponse resp) {
-		Content content = contentService.showContentById(contentId);
-		List <ContentVote> contentVote = 
+	@GetMapping("votes")
+	public List<ContentVote>showAllVotes() {
+		return voteService.index();
 	}
 	
 	
-	 @PostMapping("{contentId}/votes")
-	    public ResponseEntity<ContentVote> create(@PathVariable int contentId, @RequestBody ContentVote vote, String username) {
-	        ContentVote createdVote = voteService.create(username, contentId, vote);
-	        if (createdVote != null) {
-	            return ResponseEntity.status(HttpStatus.CREATED).body(createdVote);
-	        }
-	        return ResponseEntity.badRequest().build();
-	    }
-	
-	
+	@GetMapping("content/{content_id}/votes")
+	public List<ContentVote> showVotesByContentId(@PathVariable("contentId") int contentId, HttpServletResponse resp) {
+		Content content = contentService.showContentById(contentId);
+		return voteService.showVotesByContentId(contentId);
+	}
+
+	@PostMapping("{contentId}/votes")
+	public ResponseEntity<ContentVote> create(@PathVariable int contentId, @RequestBody ContentVote vote,
+			Principal principal) {
+		ContentVote createdVote = voteService.create(principal.getName(), contentId, vote);
+		if (createdVote != null) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(createdVote);
+		}
+		return ResponseEntity.badRequest().build();
+	}
+
+	@PutMapping("content/{content_id}/votes/{vote_id}")
+	public ContentVote updateContentVote(Principal principal, @RequestBody ContentVote contentVote,
+			@PathVariable("contentId") int contentId, @PathVariable("voteId") int voteId, HttpServletRequest req,
+			HttpServletResponse resp) {
+		try {
+			contentVote = voteService.update(principal.getName(), contentId, voteId, contentVote);
+			if (contentVote != null) {
+				resp.setStatus(201);
+			}
+		} catch (Exception e) {
+			resp.setStatus(400);
+			e.printStackTrace();
+			contentVote = null;
+		}
+		return contentVote;
+	}
 }
